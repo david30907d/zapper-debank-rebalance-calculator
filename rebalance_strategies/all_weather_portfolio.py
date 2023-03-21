f2b24aaf5802e78d83e64904e1da85a6387dfc1d
davidtnfsh <davidtnfsh@gmail.com>
2023-03-21 16:09:55 +0800

    [fix](rebalance): fix permanent portfolio

--- rebalance_strategies/all_weather_portfolio.py
+++ rebalance_strategies/all_weather_portfolio.py
@@ -1,4 +1,4 @@
-from utils.position import skip_rebalance_if_position_too_small
+from rebalance_strategies.utils import show_rebalance_suggestions
 
 
 def all_weather_portfolio(category, portfolio, net_worth):
@@ -12,19 +12,4 @@ def all_weather_portfolio(category, portfolio, net_worth):
         "non_us_developed_market_stocks": 0.06,
         "non_us_emerging_market_stocks": 0.03,
     }
-    target_sum = net_worth * target_asset_allocation[category]
-    print(
-        f"Current {category}: {portfolio['sum']:.2f}",
-        f"Target Sum: {target_sum:.2f}",
-        f"Investment Shift: {(portfolio['sum']-target_sum)/net_worth:.2f}, should be lower than 0.05",
-    )
-    diffrence = target_sum - portfolio["sum"]
-    for symbol, position_obj in sorted(
-        portfolio["portfolio"].items(), key=lambda x: -x[1]["worth"]
-    ):
-        balanceUSD = position_obj["worth"]
-        if skip_rebalance_if_position_too_small(balanceUSD):
-            continue
-        print(
-            f"Suggestion: modify this amount of USD: {diffrence * balanceUSD / portfolio['sum']:.2f} for position {symbol}, current worth: {balanceUSD:.2f}, percentage: {balanceUSD/net_worth:.2f}"
-        )
+    show_rebalance_suggestions(category, portfolio, net_worth, target_asset_allocation)
--- rebalance_strategies/permanent_portfolio.py
+++ rebalance_strategies/permanent_portfolio.py
@@ -1,20 +1,19 @@
-from utils.position import skip_rebalance_if_position_too_small
+from rebalance_strategies.utils import show_rebalance_suggestions
 
 
 def permenant_portfolio(category, portfolio, net_worth):
-    target_sum = net_worth * 0.25
-    print(
-        f"Current {category}: {portfolio['sum']:.2f}",
-        f"Target Sum: {target_sum:.2f}",
-        f"Investment Shift: {(portfolio['sum']-target_sum)/net_worth:.2f}, should be lower than 0.05",
-    )
-    diffrence = target_sum - portfolio["sum"]
-    for symbol, position_obj in sorted(
-        portfolio["portfolio"].items(), key=lambda x: -x[1]["worth"]
-    ):
-        balanceUSD = position_obj["worth"]
-        if skip_rebalance_if_position_too_small(balanceUSD):
-            continue
-        print(
-            f"Suggestion: modify this amount of USD: {diffrence * balanceUSD / portfolio['sum']:.2f} for position {symbol}, current worth: {balanceUSD:.2f}, percentage: {balanceUSD/net_worth:.2f}"
-        )
+    """
+    stock total allocation should be 25%
+    Apply all weather portfolio's allocation for large_cap_us_stocks, small_cap_us_stocks, non_us_developed_market_stocks, non_us_emerging_market_stocks these 4 categories
+    """
+    target_asset_allocation = {
+        "long_term_bond": 0.25,
+        "intermediate_term_bond": 0.25,
+        "commodities": 0,
+        "gold": 0.25,
+        "large_cap_us_stocks": 0.15,
+        "small_cap_us_stocks": 0.025,
+        "non_us_developed_market_stocks": 0.05,
+        "non_us_emerging_market_stocks": 0.025,
+    }
+    show_rebalance_suggestions(category, portfolio, net_worth, target_asset_allocation)
