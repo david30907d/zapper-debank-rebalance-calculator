902180565ea0fc557140c8aaa5e68cc574746f7e
davidtnfsh <davidtnfsh@gmail.com>
2023-05-15 12:19:00 +0100

    [refactor]: handle twd's network issue

--- utils/exchange_rate.py
+++ utils/exchange_rate.py
@@ -4,6 +4,9 @@ import requests
 def get_exrate(currency_code_name) -> float:
     # credit to https://tw.rter.info/howto_currencyapi.php
     # thanks a lot
-    resp = requests.get("https://tw.rter.info/capi.php")
-    currency = resp.json()
-    return currency[currency_code_name]["Exrate"]
+    try:
+        resp = requests.get("https://tw.rter.info/capi.php")
+        currency = resp.json()
+        return currency[currency_code_name]["Exrate"]
+    except Exception:
+        return 30.4
