04f1b07ce8f7620f788a5da72570941d8af05bf2
davidtnfsh <davidtnfsh@gmail.com>
2023-08-15 17:53:52 +0800

    [feat](debank): added symbol for debank endpoint

--- routes.py
+++ routes.py
@@ -8,14 +8,13 @@ def get_debank_data():
     token_metadata_table: dict[str, dict] = {}
     for position in evm_positions["data"]["result"]["data"]:
         for portfolio_item in position["portfolio_item_list"]:
-            for token in portfolio_item["detail"].get("reward_token_list", []):
+            for token in portfolio_item["detail"].get(
+                "reward_token_list", []
+            ) + portfolio_item["detail"].get("supply_token_list", []):
                 token_metadata_table[token["id"]] = {
                     "img": token["logo_url"],
                     "price": token["price"],
-                }
-            for token in portfolio_item["detail"].get("supply_token_list", []):
-                token_metadata_table[token["id"]] = {
-                    "img": token["logo_url"],
-                    "price": token["price"],
+                    "symbol": token["symbol"],
+                    "chain": token["chain"],
                 }
     return token_metadata_table
