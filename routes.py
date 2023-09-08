622669061ef8d92ca52cb0b6e47138f41bf1b78b
davidtnfsh <davidtnfsh@gmail.com>
2023-09-08 20:49:04 +0900

    [feat](radiant): add mapping logic to get rtoken price

--- routes.py
+++ routes.py
@@ -9,21 +9,32 @@ from tenacity import retry, stop_after_attempt, wait_fixed
 from rebalance_server.apr_utils import convert_apy_to_apr
 from rebalance_server.main import load_evm_raw_positions
 
+# v1
+# [
+#     "0xC6a58A8494E61fc4EF04F6075c4541C9664ADcC9",
+#     "0x271E3409093f7ECffFB2a1C82e5E87B2ecB3E310",
+#     "0x549caec2C863a04853Fb829aac4190E1B50df0Cc",
+#     "0xE66c4EA218Cdb8DCbCf3f605ed1aC29461CBa4b8",
+# ]
+
 
 def get_debank_data():
     evm_positions = load_evm_raw_positions(
         "debank",
         [
-            "0xC6a58A8494E61fc4EF04F6075c4541C9664ADcC9",
-            "0x271E3409093f7ECffFB2a1C82e5E87B2ecB3E310",
-            "0x549caec2C863a04853Fb829aac4190E1B50df0Cc",
-            "0xE66c4EA218Cdb8DCbCf3f605ed1aC29461CBa4b8",
+            "0xA3CDd5a4b9f5a69C5C3a297A428A10B742F1c6E1",
+            "0xBb4D0819089879d83ae13fEe71aBeAa345629389",
+            "0x0F658FC0C72A729F1B8F8444601D657D3F30Db41",
+            "0x5073bf9aE65963A5881F36560072adf5d4c6e870",
+            "0x4999AE9fDD361Ca6278B0295dd65776b4587E1aA",
+            "0x99E9cE14C807e95329a2A35aDD52683528e53231",
         ],
         useCache=True if random.random() > 0.2 else False,
     )
     token_metadata_table: dict[str, dict] = {}
     for position in evm_positions["data"]["result"]["data"]:
         for portfolio_item in position["portfolio_item_list"]:
+            protocol_name = position["name"]
             for token in portfolio_item["detail"].get(
                 "reward_token_list", []
             ) + portfolio_item["detail"].get("supply_token_list", []):
@@ -43,6 +54,16 @@ def get_debank_data():
                     token_metadata_table[
                         f"{token['chain']}:0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"
                     ] = ethPayload
+                elif token["symbol"] == "WETH" and token["chain"] != "bsc":
+                    token_metadata_table = _handle_native_token_for_each_chain(
+                        token, payload, token_metadata_table
+                    )
+                elif protocol_name == "Radiant Capital V2":
+                    print("here", token)
+                    token_metadata_table = _get_price_of_radiant_rToken(
+                        token, token_metadata_table
+                    )
+
     return token_metadata_table
 
 
@@ -160,3 +181,37 @@ def _fetch_sushiswap_APR_composition(pool_addr: str, ratio: float) -> float:
 
     result["Swap Fee"]["APR"] = pool_json["feeApr1d"] * ratio
     return result
+
+
+def _handle_native_token_for_each_chain(
+    token: dict, payload: dict, token_metadata_table: dict
+) -> dict:
+    ethPayload = copy.copy(payload)
+    ethPayload["symbol"] = "ETH"
+    ethPayload[
+        "img"
+    ] = "https://static.debank.com/image/token/logo_url/eth/935ae4e4d1d12d59a99717a24f2540b5.png"
+    token_metadata_table[
+        f"{token['chain']}:0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"
+    ] = ethPayload
+    return token_metadata_table
+
+
+def _get_price_of_radiant_rToken(token: dict, token_metadata_table: dict) -> dict:
+    mapping_from_native_token_to_rtoken = {
+        "0xfd086bc7cd5c481dcc9c85ebe478a1c0b69fcbb9": "0xd69d402d1bdb9a2b8c3d88d98b9ceaf9e4cd72d9",
+        "0xff970a61a04b1ca14834a43f5de4533ebddb5cc8": "0x48a29e756cc1c097388f3b2f3b570ed270423b3d",
+        "0xda10009cbd5d07dd0cecc66161fc93d7c9000da1": "0x0d914606f3424804fa1bbbe56ccc3416733acec6",
+        "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee": "0x0df5dfd95966753f01cb80e76dc20ea958238c46",
+        "0x5979d7b546e38e414f7e9822514be443a4800529": "0x42c248d137512907048021b30d9da17f48b5b7b2",
+        "0x912ce59144191c1204e64559fe8253a0e49e6548": "0x2dade5b7df9da3a7e1c9748d169cd6dff77e3d01",
+        "0x3082cc23568ea640225c2467653db90e9250aaa0": "0x3082cc23568ea640225c2467653db90e9250aaa0",
+        "0x2f2a2543b76a4166549f7aab2e75bef0aefc5b0f": "0x727354712bdfcd8596a3852fd2065b3c34f4f770",
+    }
+    native_token_addr = token["id"]
+    rtoken_addr = mapping_from_native_token_to_rtoken[native_token_addr]
+    if native_token_addr in mapping_from_native_token_to_rtoken:
+        token_metadata_table[f'{token["chain"]}:{rtoken_addr}'] = token_metadata_table[
+            f'{token["chain"]}:{native_token_addr}'
+        ]
+    return token_metadata_table
